53b6bdca0ca4532457926559fa75a55837b1aa08
Wendell <wendellmorais3319@gmail.com>
2022-07-21 09:40:17 -0300

    styles: alterando fonte global

--- src/global/globalStyle.ts
+++ src/global/globalStyle.ts
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        font-family: 'Open Sans', sans-serif;
+        font-family: 'Montserrat', sans-serif;
     }
 `;
 
