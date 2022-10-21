daee5b4c9f55a7ae0ecfc6693a7d6cea4d958d58
wendellmoraisz <wendellmorais3319@gmail.com>
2022-10-20 22:20:42 -0300

    criando usuário padrao

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import Home from './pages/Home';
 
 
 const App = () => {
+
+  const createDefaultUser = async () => {
+
+    try {
+      const response = await fetch("http://localhost:3333/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          user: "admin",
+          password: "12345",
+        }),
+      });
+      const json = await response.json();
+      console.log(json);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  createDefaultUser();
+
   return (
     <Home />
   );
